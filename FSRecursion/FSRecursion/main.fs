9ef3fff6d12e6e4e14c7243d99261cd778cb1d11
Jordan Ell <jordan.ell7@gmail.com>
2011-10-20 12:15:25 -0700

    Assignment 2 parts 1.1 and 1.2

---
+++ FSRecursion/FSRecursion/main.fs
@@ -0,0 +1,17 @@
+﻿module main
+open System
+
+let testList = [1;2;3;]
+
+let main =
+    Console.WriteLine("Running FSRecurssion...");
+    Console.WriteLine("Part1, Excercise 1")
+    let a = part1.AllRotations testList
+    printfn "%A" a
+    Console.WriteLine("Excercise 1 done.");
+    Console.WriteLine("Part1, Excercise 2")
+    let a = part1.AllPerms testList
+    printfn "%A" a
+    Console.WriteLine("Excercise 2 done.");
+    Console.ReadKey()
+
---
+++ FSRecursion/FSRecursion/part1.fs
@@ -0,0 +1,49 @@
+﻿// CSC 330 Assignment 2
+module part1
+open System
+
+//////////////////////////////////////////////////////////////
+
+// Does the actual recussion for AllRotations
+let rec RecursiveRotations list appendlist length = 
+    if length = 0 then
+        appendlist
+    else
+        let newList = List.append (List.tail list) ([List.head list])
+        RecursiveRotations newList (newList :: appendlist) (length-1)
+
+// All Rotations function
+let AllRotations list =
+   let length = List.length list
+   RecursiveRotations list [] length
+
+/////////////////////////////////////////////////////////////
+
+// Distrubutes e over all possitions of L
+let distrib e list =
+    let rec aux pre post = 
+        seq {
+            match post with
+            | [] -> yield (list @ [e])
+            | h::t -> yield (List.rev pre @ [e] @ post)
+                      yield! aux (h::pre) t 
+        }
+    aux [] list
+
+// Does the actual recursion for All Permutations
+let rec RecursivePermutations = function 
+    | [] -> Seq.singleton []
+    | h::t -> Seq.collect (distrib h) (RecursivePermutations t)
+
+// All Permutations function
+let AllPerms list  = 
+    Seq.toList (RecursivePermutations list)
+
+////////////////////////////////////////////////////////////
+
+let OddEven list =
+    
+
+
+
+
