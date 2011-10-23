e9af2a5cb6815ca58f6f116deff7f2a6218a51cb
Jordan Ell <jordan.ell7@gmail.com>
2011-10-23 15:13:20 -0700

    Added part 2 and have finished section 6 and 8

--- FSRecursion/FSRecursion/main.fs
+++ FSRecursion/FSRecursion/main.fs
@@ -5,6 +5,8 @@ let testList = [1;3;5;]
 let testList2 = [2;4;6]
 let testList3 = [1;6;3;5;2;4;]
 
+let vals = [ ("Num", 37); ("x", 42); ("y", -1); ("Num", 11) ]
+
 let main =
     Console.WriteLine("Running FSRecurssion...");
     Console.WriteLine("Part1, Excercise 1")
@@ -27,5 +29,17 @@ let main =
     let a = part1.MergeSort testList3
     printfn "%A" a
     Console.WriteLine("Excercise 5 done.");
+
+    /////////////////////////////////////////////////////
+
+    Console.WriteLine("Part2, Excercise 6")
+    let a = part2.LookUp vals "Foo"
+    printfn "%A" a
+    Console.WriteLine("Excercise 6 done.");
+     Console.WriteLine("Part2, Excercise 8")
+    let a = part2.Update vals "foo" 666
+    printfn "%A" a
+    Console.WriteLine("Excercise 8 done.");
+
     Console.ReadKey()
 
---
+++ FSRecursion/FSRecursion/part2.fs
@@ -0,0 +1,31 @@
+﻿// CSC 330 Assignment 2
+module part2
+open System
+
+//////////////////////////////////////////////////////////////
+
+// Look Up function
+let rec LookUp list value =
+    match list with
+    | [] -> None
+    | hd::tl -> if (fst (List.head list) = value) then
+                    Some (snd  (List.head list))
+                else
+                    LookUp (List.tail list) value
+
+
+/////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////
+
+// Update function
+let rec Update list string num =
+    match list with
+    | [] -> list
+    | hd::tl -> if (fst (List.head list) = string) then
+                    [(string, num)] @ (List.tail list)
+                else
+                    [(List.head list)] @ Update (List.tail list) string num
+
+ 
+/////////////////////////////////////////////////////////////
