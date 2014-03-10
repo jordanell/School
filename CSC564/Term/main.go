c330b7cb00fca8d57d492a378576b8b5d5f34bce
Jordan Ell <jordan.ell7@gmail.com>
2014-03-10 10:51:58 -0700

    CSC564 Set up skeleton for go chat server.

---
+++ CSC564/Term/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"./chat"
+)
+
+func main() {
+	server := chat.NewServer("/entry")
+	go server.Listen()
+	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	for _, path := range []string{"lib", "js"} {
+		http.Handle("/"+path+"/", http.StripPrefix("/"+path+"/", http.FileServer(http.Dir(path))))
+	}
+	
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
